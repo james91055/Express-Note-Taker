b3b7895ba1a3a0400cbee4d2b7839f6e9fb3a043
James <james91055@gmail.com>
2022-07-17 11:15:36 -0700

    update apis

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { env } = require("process");
+const notesData = require("./db/db.json");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -10,53 +12,69 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "public"));
 
 // GET Route for homepage
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
-// GET Route for feedback page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
+// GET Route for notes page
 app.get("/api/notes", (req, res) => {
   // Send a message to the client
   res.json(`${req.method} request received to get notes`);
 
   // Log our request to the terminal
   console.info(`${req.method} request received to get notes`);
+  return res.json(JSON.parse(fs.readFileSync("./db/db.json")));
 });
 
-// POST request to add a review
+// POST request to add a notes
 app.post("/api/notes", (req, res) => {
   // Log that a POST request was received
-  console.info(`${req.method} request received to add a review`);
+  console.info(`${req.method} request received to add a note`);
 
   // Destructuring assignment for the items in req.body
-  const { title, text } = req.body;
+  const { noteTitle, noteText } = req.body;
 
   // If all the required properties are present
-  if (title && text) {
+  if (noteTitle && noteText) {
     // Variable for the object we will save
-    const newNotes = {
-      title,
-      text,
+    const newReview = {
+      noteTitle,
+      noteText,
     };
 
-    // Convert the data to a string so we can save it
-    const notesString = JSON.stringify(newNotes);
+    // Obtain existing reviews
+    fs.readFile("./db/reviews.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedReviews = JSON.parse(data);
 
-    // Write the string to a file
-    fs.writeFile(`./db/${newNotes.product}.json`, notesString, (err) =>
-      err
-        ? console.error(err)
-        : console.log(
-            `Review for ${newNotes.product} has been written to JSON file`
-          )
-    );
+        // Add a new review
+        parsedReviews.push(newReview);
+
+        // Write updated reviews back to the file
+        fs.writeFile(
+          "./db/reviews.json",
+          JSON.stringify(parsedReviews, null, 2),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info("Successfully updated reviews!")
+        );
+      }
+    });
 
     const response = {
       status: "success",
-      body: newNotes,
+      body: newReview,
     };
 
     console.log(response);
